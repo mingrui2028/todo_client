8ffda479bf1db79dfdbd2ec2d3a780ce5f58448f
mingrui2028 <146952511+mingrui2028@users.noreply.github.com>
2024-03-26 22:25:13 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001'; 
+//const BACKEND_ROOT_URL = 'http://localhost:3001'; 
 
 import { Todos } from "./class/Todos.js"; 
 const todos = new Todos(BACKEND_ROOT_URL); 
